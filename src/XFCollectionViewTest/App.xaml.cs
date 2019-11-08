53b9f7d4777304229dd952289cd4e09e1085dada
Ricardo Prestes <ricardo@forlogic.net>
2019-11-08 15:03:37 -0300

    Adding NavigationPage

--- src/XFCollectionViewTest/App.xaml.cs
+++ src/XFCollectionViewTest/App.xaml.cs
@@ -9,7 +9,7 @@ namespace XFCollectionViewTest
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
